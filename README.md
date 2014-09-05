4d4bed4d538526c199e4658a0a12fd026157b30f
Victor Kareh <vkareh@vkareh.net>
2014-09-05 15:06:14 -0400

    Added support for word stemming

--- README.md
+++ README.md
@@ -43,19 +43,33 @@ db.users.find({ firstName: { $in: ['victor', 'vic', 'vick'] } })
 ```
 Thus making it more likely that we will find the correct person.
 
-A limitation of this approach is that word stemming happens at the database
-level, whereas synonym replacement happens at the application level. This means
-that synonyms might not be replaced for different conjugations of a word. For
-example, searching for the word "_messaging_" might not yield any synonyms,
-since it's a conjugation of the word "_message_", whereas searching for the word
-"_message_" might yield "_message_", "_communication_", "_memo_",
-"_notification_", which would be more useful.
+In addition to basic word replacement, this plugin includes an optional word
+stemmer to account for different conjugations of words. This is most useful when
+using a dictionary for common word replacements (as opposed to nicknames or
+acronyms). To use the word stemming feature, pass a `{stem: true}` option to the
+plugin:
+```javascript
+MySchema.plugin(mongooseSynonyms, {
+  dictionaryName: 'common-words',
+  dictionary: require('./myDictionaryOfCommonWords.json'),
+  stem: true
+});
+```
 
-Another limitation is that this method would give each word the same weight when
-analyzing the query. The case could be made that the original word should be
-preferred over any of its synonyms, but MongoDB has no way of differentiating
-between them when executing the query.
+When passing a custom dictionary (be it an inline JSON object or a required
+dictionary file), you should always pass a `dictionaryName` property along with
+it so that the plugin knows to cache it (and thus avoid preparing the dictionary
+from scratch for every database query).
 
+Limitations
+-----------
+A limitation of this plugin is that this method would give each word the same
+weight when analyzing the query. The case could be made that the original word
+should be preferred over any of its synonyms, but MongoDB has no way of
+differentiating between them when executing the query.
+
+Dictionaries
+------------
 Some sample (but usable) dictionaries are included. Feel free to add more
 entries to them and pull request, or to add entirely new dictionaries from free
 and open sources.
--- test/mongoose-synonyms.js
+++ test/mongoose-synonyms.js
@@ -1,87 +1,125 @@
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
 var mongooseSynonyms = require('../lib/mongoose-synonyms');
+var _ = require('underscore');
 
 var schema, dictionary, conditions;
 describe('Mongoose synonyms plugin', function() {
   before(function() {
-    schema = mongoose.Schema({ firstName: String });
+    schema = mongoose.Schema({ firstName: String, description: String });
   });
 
-  it('should add hooks to static schema methods', function() {
-    expect(schema).to.not.contain.property('hook');
-    mongooseSynonyms(schema, {
-      dictionary: 'nicknames'
+  describe('without word stemming', function() {
+    it('should add hooks to static schema methods', function() {
+      expect(schema).to.not.contain.property('hook');
+      mongooseSynonyms(schema, {
+        dictionary: 'nicknames',
+        fields: ['firstName']
+      });
+      expect(schema).to.contain.property('pre');
     });
-    expect(schema).to.contain.property('pre');
+
+    describe('.prepareDictionary', function() {
+      before(function() {
+        dictionary = mongooseSynonyms.prepareDictionary('nicknames');
+      });
+
+      it('should load dictionary file', function() {
+        expect(dictionary).to.be.ok;
+      });
+
+      it('should map words to synonyms', function() {
+        expect(dictionary).to.contain.key('victor');
+        expect(dictionary.victor).to.contain.members(['victor', 'vick', 'vic']);
+      });
+
+      it('should map synonyms to words', function() {
+        expect(dictionary).to.contain.key('vic');
+        expect(dictionary.vic).to.contain.members(['victor', 'vick', 'vic']);
+      });
+
+      it('should load dictionary file from path', function() {
+        var dict = mongooseSynonyms.prepareDictionary('universities', require('../dictionaries/universities.json'));
+        expect(dict).to.be.ok;
+        expect(dict).to.contain.keys(['university of michigan', 'umich', 'u of m']);
+        var cached = mongooseSynonyms.prepareDictionary('universities');
+        expect(cached).to.equal(dict);
+      });
+    });
+
+    describe('.addSynonyms', function() {
+      before(function(done) {
+        var addSynonyms = mongooseSynonyms.addSynonyms(dictionary, ['firstName', 'alias', '$text.$search', 'foo']);
+        conditions = {
+          firstName: 'victor',
+          alias: { $in: ['victor', 'david', 'foo'] },
+          $text: { $search: 'victor david' },
+          bar: 'baz'
+        };
+        addSynonyms(done, conditions);
+      });
+
+      it('should replace string queries', function() {
+        expect(conditions.firstName).to.be.ok;
+        expect(conditions.firstName).to.have.keys('$in');
+        expect(conditions.firstName.$in).to.be.an.instanceOf(Array);
+        expect(conditions.firstName.$in).to.contain.members(['victor', 'vick', 'vic']);
+      });
+
+      it('should replace array queries', function() {
+        expect(conditions.alias).to.be.ok;
+        expect(conditions.alias).to.have.keys('$in');
+        expect(conditions.alias.$in).to.be.an.instanceOf(Array);
+        expect(conditions.alias.$in).to.contain.members(['victor', 'vick', 'vic', 'david', 'dave', 'davy', 'vida', 'foo']);
+      });
+
+      it('should replace text search queries', function() {
+        expect(conditions.$text.$search).to.be.ok;
+        expect(conditions.$text.$search).to.contain('victor');
+        expect(conditions.$text.$search).to.contain('vick');
+        expect(conditions.$text.$search).to.contain('vic');
+        expect(conditions.$text.$search).to.contain('david');
+        expect(conditions.$text.$search).to.contain('dave');
+        expect(conditions.$text.$search).to.contain('davy');
+        expect(conditions.$text.$search).to.contain('vida');
+      });
+
+      it('should not replace words not in dictionary', function() {
+        expect(conditions.bar).to.be.ok;
+        expect(conditions.bar).to.equal('baz');
+      });
+    });
   });
 
-  describe('.prepareDictionary', function() {
+  describe('with word stemming', function() {
+    var pangram = {
+      'quick': ['rapid', 'fast'],
+      'jumps': ['hops', 'skips'],
+      'over': ['above'],
+      'lazy': ['idle', 'sluggish'],
+      'dog': ['canine']
+    };
     before(function() {
-      dictionary = mongooseSynonyms.prepareDictionary('nicknames');
+      mongooseSynonyms(schema, {
+        dictionaryName: 'pangram',
+        dictionary: pangram,
+        stem: true
+      });
     });
 
-    it('should load dictionary file', function() {
-      expect(dictionary).to.be.ok;
+    it('should prepare a dictionary with stemmed keys', function() {
+      var dict = mongooseSynonyms.prepareDictionary('pangram');
+      expect(dict).to.contain.keys(['quick', 'rapid', 'fast', 'jump', 'hop', 'skip', 'ov', 'abov', 'lazy', 'idl', 'slug', 'dog', 'canin']);
     });
 
-    it('should map words to synonyms', function() {
-      expect(dictionary).to.contain.key('victor');
-      expect(dictionary.victor).to.contain.members(['victor', 'vick', 'vic']);
-    });
-
-    it('should map synonyms to words', function() {
-      expect(dictionary).to.contain.key('vic');
-      expect(dictionary.vic).to.contain.members(['victor', 'vick', 'vic']);
-    });
-
-    it('should load dictionary file from path', function() {
-      var dict = mongooseSynonyms.prepareDictionary(require('../dictionaries/universities.json'));
-      expect(dict).to.be.ok;
-      expect(dict).to.contain.keys(['University of Michigan', 'UMICH', 'U of M']);
-    });
-  });
-
-  describe('.addSynonyms', function() {
-    before(function(done) {
-      var addSynonyms = mongooseSynonyms.addSynonyms(dictionary, ['firstName', 'alias', '$text.$search', 'foo']);
-      conditions = {
-        firstName: 'victor',
-        alias: { $in: ['victor', 'david', 'foo'] },
-        $text: { $search: 'victor david' },
-        bar: 'baz'
-      };
-      addSynonyms(done, conditions);
-    });
-
-    it('should replace string queries', function() {
-      expect(conditions.firstName).to.be.ok;
-      expect(conditions.firstName).to.have.keys('$in');
-      expect(conditions.firstName.$in).to.be.an.instanceOf(Array);
-      expect(conditions.firstName.$in).to.contain.members(['victor', 'vick', 'vic']);
-    });
-
-    it('should replace array queries', function() {
-      expect(conditions.alias).to.be.ok;
-      expect(conditions.alias).to.have.keys('$in');
-      expect(conditions.alias.$in).to.be.an.instanceOf(Array);
-      expect(conditions.alias.$in).to.contain.members(['victor', 'vick', 'vic', 'david', 'dave', 'davy', 'vida', 'foo']);
-    });
-
-    it('should replace text search queries', function() {
-      expect(conditions.$text.$search).to.be.ok;
-      expect(conditions.$text.$search).to.contain('victor');
-      expect(conditions.$text.$search).to.contain('vick');
-      expect(conditions.$text.$search).to.contain('vic');
-      expect(conditions.$text.$search).to.contain('david');
-      expect(conditions.$text.$search).to.contain('dave');
-      expect(conditions.$text.$search).to.contain('davy');
-      expect(conditions.$text.$search).to.contain('vida');
-    });
-
-    it('should not replace words not in dictionary', function() {
-      expect(conditions.bar).to.be.ok;
-      expect(conditions.bar).to.equal('baz');
+    it('should get synonms based on word stem', function() {
+      var dict = mongooseSynonyms.prepareDictionary('pangram');
+      var addSynonyms = mongooseSynonyms.addSynonyms(dict, ['$text.$search'], true);
+      conditions = { $text: { $search: 'The rapidly hopping fox skipped over the idling dog.' } };
+      addSynonyms(function() {
+        expect(conditions).to.be.ok;
+        expect(conditions.$text.$search).to.equal('The quick rapid fast jumps hops skips fox jumps hops skips over above the lazy idle sluggish dog.');
+      }, conditions);
     });
   });
 });
